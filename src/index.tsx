f4727f5e14d71142bdc87ba3934ead14abaa5cd1
Samuel Briole <samuel.briole@gmail.com>
2022-10-10 17:35:23 +0200

    add error guard on index

--- src/index.tsx
+++ src/index.tsx
@@ -58,6 +58,9 @@ function Updaters() {
 }
 
 const container = document.getElementById('root')
+if (!container) {
+  throw new Error("Couldn't find #root element")
+}
 const root = createRoot(container)
 root.render(
   <ContextProviders>
